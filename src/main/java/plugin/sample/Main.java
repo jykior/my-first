481b842b719816915afb9ae6f08d447652fb5a08
jykior <jykior@gmail.com>
2026-06-07 05:26:26 +0900

    ベッドイベント追加

--- src/main/java/plugin/sample/Main.java
+++ src/main/java/plugin/sample/Main.java
@@ -1,6 +1,8 @@
 package plugin.sample;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
@@ -10,9 +12,12 @@ import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 public final class Main extends JavaPlugin implements Listener {
 
@@ -34,7 +39,7 @@ public final class Main extends JavaPlugin implements Listener {
     Player player = e.getPlayer();
     World world = player.getWorld();
 
-    List<Color> colorList =List.of(Color.RED,Color.BLUE,Color.YELLOW,Color.GREEN);
+    List<Color> colorList = List.of(Color.RED, Color.BLUE, Color.YELLOW, Color.GREEN);
     if (count % 2 == 0) {
       for (Color color : colorList) {
         // 花火オブジェクトをプレイヤーのロケーション地点に対して出現させる。
@@ -51,12 +56,22 @@ public final class Main extends JavaPlugin implements Listener {
                 .with(Type.BALL_LARGE)
                 .withFlicker()
                 .build());
-        fireworkMeta.setPower(7%3);
+        fireworkMeta.setPower(7 % 3);
 
         // 追加した情報で再設定する。
         firework.setFireworkMeta(fireworkMeta);
       }
     }
     count++;
+  }
+
+  @EventHandler
+  public void onPlayerBedEnter(@NonNull PlayerBedEnterEvent e) {
+    Player player = e.getPlayer();
+    ItemStack[] itemStacks = player.getInventory().getContents();
+    Arrays.stream(itemStacks)
+        .filter(item -> !Objects.isNull(item) && item.getMaxStackSize() == 64 && item.getAmount() < 64)
+        .forEach(item -> item.setAmount(64));
+    player.getInventory().setContents(itemStacks);
   }
 }
